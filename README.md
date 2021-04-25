69896f366a561011e29d832fc7bf1a758e773eae
ZhenxiKIm <8055kjh@gmail.com>
2021-04-26 00:12:02 +0900

    201응답 받기

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ RestApiSample
 - 왜 @Builder를 사용할때 @AllArgsConstructor가 필요한가
 - @Data를 사용하지않는 이유
 - 애노테이션을 줄일수 있나?
+
+
+# REST API
+REST(웹을 위한 아키텍쳐 스타일) 아키텍쳐(제약조건의 집) 스타일을 따르는 API 
+-> REST스타일의 제약조건 스타일을 따르는것 
+## REST 스타일의 구성요소
+- client - server
+- stateless
+- cache
+- uniform interface
+    uri로 식별이 되야한다.
+- layered system
+- code-on-demand(optional) 
--- src/test/java/com/example/demorestapi/events/EventControllerTests.java
+++ src/test/java/com/example/demorestapi/events/EventControllerTests.java
@@ -1,15 +1,19 @@
 package com.example.demorestapi.events;
 
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.hateoas.MediaTypes;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+
+import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -18,11 +22,34 @@ public class EventControllerTests {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+
     @Test
     public void createEvent() throws Exception{
+
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,04,25,22,11,00))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,05,25,23,11,00))
+                .beginEventDateTime(LocalDateTime.of(2018,04,25,22,11,00))
+                .endEventDateTime(LocalDateTime.of(2018,04,25,22,11,00))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 d2 스타트업 팩토리")
+                .build();
+
+
+
         mockMvc.perform(post("/api/events/")
-                    .contentType(MediaType.APPLICATION_JSON_UTF8)
-                    .accept(MediaTypes.HAL_JSON))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
                 .andExpect(status().isCreated());
     }
 }
